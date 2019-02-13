8c3c917c2363e26ad6053a63bb6c05776fff6857
Adam Partyka <apartyka@updox.com>
2019-02-13 14:34:03 -0500

    [HB-55] we do not need the angular page for this demo as it is the web app we’re calling to

--- src/assets/ng/ng.js
+++
@@ -1,7 +0,0 @@
-var app = angular.module('myShoppingList', []);
-app.controller('myCtrl', function($scope) {
-    $scope.products = window.products = ['Milk', 'Bread', 'Cheese'];
-    $scope.addItem = function() {
-        $scope.products.push($scope.addMe);
-    };
-});
